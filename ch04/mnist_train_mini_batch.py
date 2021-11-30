63ea03ad9264ec0e399b77a4ea12675e6816fffc
yyhclimacool <yyhclimacool@gmail.com>
2021-11-30 23:27:36 +0800

    基于测试数据的评价

--- ch04/mnist_train_mini_batch.py
+++ ch04/mnist_train_mini_batch.py
@@ -9,23 +9,40 @@ from dataset.mnist import load_mnist
 from two_layer_net import TwoLayerNet
 
 # mini-batch的实现
+# 分epoch打印精度
 def mnist_mini_batch():
     (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
-    iter_num = 10000
+
     train_size = x_train.shape[0]
+
+    iter_num = 10000
     batch_size = 100
-    loss_record_list = []
+    lr = 0.1
+
+    iter_per_epoch = max(train_size/batch_size, 1)
+
+    #loss_record_list = []
+    train_acc_list = []
+    test_acc_list = []
     net = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
     for i in range(iter_num):
-        mini_batch = np.random.choice(train_size, batch_size)
-        x_batch = x_train[mini_batch]
-        t_batch = t_train[mini_batch]
         print("Processing " + str(i) + " of " + str(iter_num))
+
+        batch_mask = np.random.choice(train_size, batch_size)
+        x_batch = x_train[batch_mask]
+        t_batch = t_train[batch_mask]
+
         grads = net.numerical_gradient(x_batch, t_batch)
-        net.optimize(lr=0.1, grads=grads)
-        loss_record_list.append(net.loss(x_batch, t_batch))
-    for loss in loss_record_list:
-        print(loss)
+        net.optimize(lr=lr, grads=grads)
+
+        if i % iter_per_epoch == 0:
+            #loss_record_list.append(net.loss(x_train, t_train))
+            train_acc = net.accuracy(x_train, t_train)
+            test_acc = net.accuracy(x_test, t_test)
+            train_acc_list.append(train_acc)
+            test_acc_list.append(test_acc)
+    for i in range(len(train_acc_list)):
+        print("train_acc: " + str(train_acc_list[i]) + ", test_acc: " + str(test_acc_list[i]))
 
 if __name__ == "__main__":
     mnist_mini_batch()
